5d2dcd2986186d2928ac2fc17c03ecfe54cc05a6
esokoletsky <Esokoletsky@gmail.com>
2018-09-27 14:56:38 -0400

    congif test url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://esokoletsky:deskjet11@ds263642.mlab.com:63642/restaurant-travisci';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://esokoletsky:deskjet11@ds263642.mlab.com:63642/restaurant-travisci';
 exports.PORT = process.env.PORT || 8080;
