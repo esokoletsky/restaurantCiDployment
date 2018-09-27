79530e0bfb0d8517249ede4ef23003de9d3a4b43
esokoletsky <Esokoletsky@gmail.com>
2018-09-27 15:18:39 -0400

    added test database

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://esokoletsky:deskjet11@ds263642.mlab.com:63642/restaurant-travisci';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://esokoletsky:deskjet11@ds263642.mlab.com:63642/restaurant-travisci';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://esokoletsky:deskjet11@ds117423.mlab.com:17423/restaurant-travisci-test';
 exports.PORT = process.env.PORT || 8080;
